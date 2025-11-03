b189ab659e291a73d4ad316e50031bbb2e621423
SguXa <sgyxa17@list.ru>
2025-11-03 17:31:33 +0100

    Update storage.py

--- core/storage.py
+++ core/storage.py
@@ -1,24 +1,24 @@
 import logging
 import asyncpg
-from core.models import Wish as WishModel
+from core.models import Wish
 
 
 class Storage:
     def __init__(self, pool: asyncpg.Pool):
         self._pool = pool
 
-    async def list_wishes(self, user_id: int):
+    async def list_wishes(self, user_id: int) -> list[Wish]:
         async with self._pool.acquire() as conn:
-            rows = await conn.fetch("SELECT * FROM wishes WHERE user_id=$1 ORDER BY category, priority DESC", user_id)
-            return rows
+            rows = await conn.fetch("SELECT title, link, category, description, priority FROM wishes WHERE user_id=$1 ORDER BY category, priority DESC", user_id)
+            return [Wish(*row) for row in rows]
 
-    async def add_wish(self, user_id: int, wish: WishModel) -> None:
+    async def add_wish(self, user_id: int, wish: Wish) -> None:
         """
         Add a new wish to the database.
 
         Args:
             user_id (int): The ID of the user adding the wish.
-            wish (WishModel): The wish object containing details to be added.
+            wish (Wish): The wish object containing details to be added.
 
         Raises:
             asyncpg.PostgresError: If there is an error during the database operation.
