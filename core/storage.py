b3a3b3b79955254d135d40a759d74f4f3db1cf88
SguXa <sgyxa17@list.ru>
2025-11-03 17:13:44 +0100

    Update storage.py

--- core/storage.py
+++ core/storage.py
@@ -1,10 +1,10 @@
 import logging
 import asyncpg
-from core.models import Wish
+from core.models.wish import Wish
 
 
 class Storage:
-    def __init__(self, pool):
+    def __init__(self, pool: asyncpg.Pool):
         self._pool = pool
 
     async def list_wishes(self, user_id: int):
@@ -12,13 +12,26 @@ class Storage:
             rows = await conn.fetch("SELECT * FROM wishes WHERE user_id=$1 ORDER BY category, priority DESC", user_id)
             return rows
 
-    async def add_wish(self, user_id: int, wish: Wish):
+    async def add_wish(self, user_id: int, wish: Wish) -> None:
+        """
+        Add a new wish to the database.
+
+        Args:
+            user_id (int): The ID of the user adding the wish.
+            wish (Wish): The wish object containing details to be added.
+
+        Raises:
+            asyncpg.PostgresError: If there is an error during the database operation.
+        """
         try:
             async with self._pool.acquire() as conn:
                 await conn.execute(
-                    "INSERT INTO wishes (user_id, title, link, category, description, priority) VALUES ($1, $2, $3, $4, $5, $6)",
-                    user_id, wish.title, wish.link, wish.category, wish.description, wish.priority
+                    """
+                    INSERT INTO wishes (user_id, title, link, category, description, priority)
+                    VALUES ($1, $2, $3, $4, $5, $6)
+                    """,
+                    user_id, *wish.as_tuple()
                 )
         except asyncpg.PostgresError as e:
-            logging.error(f"Failed to add wish: {e}")
+            logging.error(f"Failed to add wish for user {user_id}: {e}")
             raise
